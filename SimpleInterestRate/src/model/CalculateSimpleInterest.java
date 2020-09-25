8d12c5557e71f3eb59543fc18530167feff9e663
rmcggee <ramcgee1@dmacc.edu>
2020-09-25 02:24:11 -0500

    Just a test to try to synch up

--- SimpleInterestRate/src/model/CalculateSimpleInterest.java
+++ SimpleInterestRate/src/model/CalculateSimpleInterest.java
@@ -2,30 +2,6 @@ package model;
 
 public class CalculateSimpleInterest {
 	//Ruth McGee
-	private double principalAmt;
-	private double annualInterestRatePrcnt;
-	private int yearsCount;
-	
-	// constructor
-	public CalculateSimpleInterest(double prin, double apr, int yrCt) {
-		super();
-		this.principalAmt = prin;
-		this.annualInterestRatePrcnt = apr;
-		this.yearsCount = yrCt;
-	}
-	
-	public double returnAmountEarned() {
-		double amtEarned = ((this.annualInterestRatePrcnt / 100) * this.principalAmt * this.yearsCount);
-		return amtEarned;
-	}
-	
-	
-	//getters and setters
-	public double getprincipalAmt() {
-		return this.principalAmt;
-	}
-	public void setprincipalAmt (double principalAmt) {
-		this.principalAmt = principalAmt;
 	}
 	public double getAnnualInterestRatePrcnt() {
 		return this.annualInterestRatePrcnt;
